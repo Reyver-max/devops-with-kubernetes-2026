6f93373ad5ef05945081df659bd5091a12896332
Reyver Serna <reyver.arley@gmail.com>
2026-06-18 16:09:11 +0300

    Complete exercise 1.5 project step 3

--- applications/project/index.js
+++ applications/project/index.js
@@ -1,11 +1,18 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Todo App");
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <body>
+        <h1>Todo App</h1>
+        <p>Exercise 1.5 working!</p>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(PORT, () => {
