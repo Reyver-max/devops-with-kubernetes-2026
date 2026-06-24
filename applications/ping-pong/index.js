bec3b529768967e8a545c41e29d32cb3946aa5bd
Reyver Serna <reyver.arley@gmail.com>
2026-06-24 10:52:43 +0300

    Complete exercise 1.9 more services

---
+++ applications/ping-pong/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+let counter = 0;
+
+app.get("/pingpong", (req, res) => {
+  res.send(`pong ${counter}`);
+  counter++;
+});
+
+app.listen(PORT, () => {
+  console.log(`Ping-pong app started in port ${PORT}`);
+});
